423b9f7c430b0433c1180ce9c51524fcda0df85b
Rizwan Mansuri <Rizwan@webbyfox.com>
2019-01-17 13:51:27 +0000

    Typo fix

--- djangocms_references/apps.py
+++ djangocms_references/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
-class ReferenceConfig(AppConfig):
+class ReferencesConfig(AppConfig):
     name = "djangocms_references"
-    verbose_name = _("django CMS Reference")
+    verbose_name = _("django CMS References")
