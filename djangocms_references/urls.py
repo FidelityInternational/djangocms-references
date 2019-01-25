f2ab9c5444c27312cd614ed04e56f8d88642a30f
Rizwan Mansuri <Rizwan@webbyfox.com>
2019-01-25 16:52:31 +0000

    Changed urls.py for cloud env

--- djangocms_references/urls.py
+++ djangocms_references/urls.py
@@ -7,7 +7,7 @@ from .views import ReferencesView
 app_name = "djangocms_references"
 urlpatterns = [
     url(
-        r"^(?P<content_type_id>\d+)/(?P<object_id>\d+)/$",
+        r"^references/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$",
         staff_member_required(ReferencesView.as_view()),
         name="references-index",
     )
