8359c3b019b82f2a3e9fc274e994d70aed0011f0
Josh Peng Yu <joshyu@users.noreply.github.com>
2024-03-09 15:50:29 +0000

    feat: update django 4.2 compatible for django cms 4.0.1.x branch

--- djangocms_references/compat.py
+++ djangocms_references/compat.py
@@ -1,12 +1,3 @@
-from cms import __version__ as cms_version
-
-
-try:
-    from packaging.version import Version
-except ModuleNotFoundError:
-    from distutils.version import LooseVersion as Version
-
-
 def is_versioning_installed():
     try:
         import djangocms_versioning  # noqa: F401
@@ -14,9 +5,6 @@ def is_versioning_installed():
         return False
     else:
         return True
-
-
-DJANGO_CMS_4_1 = Version(cms_version) >= Version('4.1')
 
 
 VERSIONING_INSTALLED = is_versioning_installed()
